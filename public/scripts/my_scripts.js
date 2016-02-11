59bedab93caf19e058afd4ec5b7d532eba4ccc90
Timofey Jedi <Timofey Jedi>
2016-02-11 12:00:54 +0300

    added mobile-nav, main part

---
+++ public/scripts/my_scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+	$('.menu-button').click(function(){
+		$('.mobile-nav-menu').fadeIn('fast');
+	});
+
+	$('.close').click(function(){
+		$('.mobile-nav-menu').fadeOut('fast');
+	}); 
+
+});	
